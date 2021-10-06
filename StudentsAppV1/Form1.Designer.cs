8faa617cb2dab329e05cc56e81e7f40f4ce73931
amrazizi <66872391+amrazizi@users.noreply.github.com>
2021-10-06 21:20:57 +0300

    Add files via upload

---
+++ StudentsAppV1/Form1.Designer.cs
@@ -0,0 +1,356 @@
+﻿
+namespace StudentsAppV1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNo = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.lblNo = new System.Windows.Forms.Label();
+            this.lblName = new System.Windows.Forms.Label();
+            this.cmbFaculty = new System.Windows.Forms.ComboBox();
+            this.lblFaculty = new System.Windows.Forms.Label();
+            this.lblDept = new System.Windows.Forms.Label();
+            this.cmbDept = new System.Windows.Forms.ComboBox();
+            this.picImage = new System.Windows.Forms.PictureBox();
+            this.dtDOB = new System.Windows.Forms.DateTimePicker();
+            this.lblDOB = new System.Windows.Forms.Label();
+            this.rdMale = new System.Windows.Forms.RadioButton();
+            this.rdFemale = new System.Windows.Forms.RadioButton();
+            this.grpGender = new System.Windows.Forms.GroupBox();
+            this.chkMarried = new System.Windows.Forms.CheckBox();
+            this.lblMobile = new System.Windows.Forms.Label();
+            this.txtMobile = new System.Windows.Forms.TextBox();
+            this.pnlEyeColor = new System.Windows.Forms.Panel();
+            this.lblEyeColor = new System.Windows.Forms.Label();
+            this.lblAdress = new System.Windows.Forms.Label();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.dgStudents = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.picImage)).BeginInit();
+            this.grpGender.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgStudents)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtNo
+            // 
+            this.txtNo.Location = new System.Drawing.Point(14, 25);
+            this.txtNo.Name = "txtNo";
+            this.txtNo.Size = new System.Drawing.Size(104, 20);
+            this.txtNo.TabIndex = 0;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(124, 25);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(171, 20);
+            this.txtName.TabIndex = 1;
+            // 
+            // lblNo
+            // 
+            this.lblNo.AutoSize = true;
+            this.lblNo.Location = new System.Drawing.Point(11, 9);
+            this.lblNo.Name = "lblNo";
+            this.lblNo.Size = new System.Drawing.Size(24, 13);
+            this.lblNo.TabIndex = 2;
+            this.lblNo.Text = "No:";
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(121, 9);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(38, 13);
+            this.lblName.TabIndex = 3;
+            this.lblName.Text = "Name:";
+            // 
+            // cmbFaculty
+            // 
+            this.cmbFaculty.FormattingEnabled = true;
+            this.cmbFaculty.Items.AddRange(new object[] {
+            "f1",
+            "f2",
+            "f3"});
+            this.cmbFaculty.Location = new System.Drawing.Point(301, 24);
+            this.cmbFaculty.Name = "cmbFaculty";
+            this.cmbFaculty.Size = new System.Drawing.Size(121, 21);
+            this.cmbFaculty.TabIndex = 4;
+            this.cmbFaculty.SelectedIndexChanged += new System.EventHandler(this.CmbFaculty_SelectedIndexChanged_1);
+            // 
+            // lblFaculty
+            // 
+            this.lblFaculty.AutoSize = true;
+            this.lblFaculty.Location = new System.Drawing.Point(301, 9);
+            this.lblFaculty.Name = "lblFaculty";
+            this.lblFaculty.Size = new System.Drawing.Size(44, 13);
+            this.lblFaculty.TabIndex = 5;
+            this.lblFaculty.Text = "Faculty:";
+            // 
+            // lblDept
+            // 
+            this.lblDept.AutoSize = true;
+            this.lblDept.Location = new System.Drawing.Point(428, 9);
+            this.lblDept.Name = "lblDept";
+            this.lblDept.Size = new System.Drawing.Size(33, 13);
+            this.lblDept.TabIndex = 7;
+            this.lblDept.Text = "Dept:";
+            // 
+            // cmbDept
+            // 
+            this.cmbDept.FormattingEnabled = true;
+            this.cmbDept.Location = new System.Drawing.Point(428, 24);
+            this.cmbDept.Name = "cmbDept";
+            this.cmbDept.Size = new System.Drawing.Size(121, 21);
+            this.cmbDept.TabIndex = 6;
+            // 
+            // picImage
+            // 
+            this.picImage.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.picImage.Location = new System.Drawing.Point(563, 9);
+            this.picImage.Name = "picImage";
+            this.picImage.Size = new System.Drawing.Size(100, 100);
+            this.picImage.TabIndex = 8;
+            this.picImage.TabStop = false;
+            this.picImage.Click += new System.EventHandler(this.PicImage_Click);
+            // 
+            // dtDOB
+            // 
+            this.dtDOB.Location = new System.Drawing.Point(124, 76);
+            this.dtDOB.Name = "dtDOB";
+            this.dtDOB.Size = new System.Drawing.Size(205, 20);
+            this.dtDOB.TabIndex = 9;
+            // 
+            // lblDOB
+            // 
+            this.lblDOB.AutoSize = true;
+            this.lblDOB.Location = new System.Drawing.Point(121, 60);
+            this.lblDOB.Name = "lblDOB";
+            this.lblDOB.Size = new System.Drawing.Size(33, 13);
+            this.lblDOB.TabIndex = 10;
+            this.lblDOB.Text = "DOB:";
+            // 
+            // rdMale
+            // 
+            this.rdMale.AutoSize = true;
+            this.rdMale.Location = new System.Drawing.Point(11, 15);
+            this.rdMale.Name = "rdMale";
+            this.rdMale.Size = new System.Drawing.Size(48, 17);
+            this.rdMale.TabIndex = 11;
+            this.rdMale.TabStop = true;
+            this.rdMale.Text = "Male";
+            this.rdMale.UseVisualStyleBackColor = true;
+            // 
+            // rdFemale
+            // 
+            this.rdFemale.AutoSize = true;
+            this.rdFemale.Location = new System.Drawing.Point(63, 15);
+            this.rdFemale.Name = "rdFemale";
+            this.rdFemale.Size = new System.Drawing.Size(59, 17);
+            this.rdFemale.TabIndex = 12;
+            this.rdFemale.TabStop = true;
+            this.rdFemale.Text = "Female";
+            this.rdFemale.UseVisualStyleBackColor = true;
+            // 
+            // grpGender
+            // 
+            this.grpGender.Controls.Add(this.rdMale);
+            this.grpGender.Controls.Add(this.rdFemale);
+            this.grpGender.Location = new System.Drawing.Point(335, 59);
+            this.grpGender.Name = "grpGender";
+            this.grpGender.Size = new System.Drawing.Size(133, 38);
+            this.grpGender.TabIndex = 13;
+            this.grpGender.TabStop = false;
+            this.grpGender.Text = "Gender";
+            // 
+            // chkMarried
+            // 
+            this.chkMarried.AutoSize = true;
+            this.chkMarried.Location = new System.Drawing.Point(476, 74);
+            this.chkMarried.Name = "chkMarried";
+            this.chkMarried.Size = new System.Drawing.Size(61, 17);
+            this.chkMarried.TabIndex = 14;
+            this.chkMarried.Text = "Married";
+            this.chkMarried.UseVisualStyleBackColor = true;
+            // 
+            // lblMobile
+            // 
+            this.lblMobile.AutoSize = true;
+            this.lblMobile.Location = new System.Drawing.Point(11, 58);
+            this.lblMobile.Name = "lblMobile";
+            this.lblMobile.Size = new System.Drawing.Size(41, 13);
+            this.lblMobile.TabIndex = 16;
+            this.lblMobile.Text = "Mobile:";
+            // 
+            // txtMobile
+            // 
+            this.txtMobile.Location = new System.Drawing.Point(14, 74);
+            this.txtMobile.Name = "txtMobile";
+            this.txtMobile.Size = new System.Drawing.Size(104, 20);
+            this.txtMobile.TabIndex = 15;
+            // 
+            // pnlEyeColor
+            // 
+            this.pnlEyeColor.BackColor = System.Drawing.Color.Red;
+            this.pnlEyeColor.Location = new System.Drawing.Point(563, 135);
+            this.pnlEyeColor.Name = "pnlEyeColor";
+            this.pnlEyeColor.Size = new System.Drawing.Size(100, 23);
+            this.pnlEyeColor.TabIndex = 17;
+            this.pnlEyeColor.Click += new System.EventHandler(this.PnlEyeColor_Click);
+            // 
+            // lblEyeColor
+            // 
+            this.lblEyeColor.AutoSize = true;
+            this.lblEyeColor.Location = new System.Drawing.Point(560, 119);
+            this.lblEyeColor.Name = "lblEyeColor";
+            this.lblEyeColor.Size = new System.Drawing.Size(54, 13);
+            this.lblEyeColor.TabIndex = 18;
+            this.lblEyeColor.Text = "Eye color:";
+            // 
+            // lblAdress
+            // 
+            this.lblAdress.AutoSize = true;
+            this.lblAdress.Location = new System.Drawing.Point(11, 107);
+            this.lblAdress.Name = "lblAdress";
+            this.lblAdress.Size = new System.Drawing.Size(48, 13);
+            this.lblAdress.TabIndex = 20;
+            this.lblAdress.Text = "Address:";
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.Location = new System.Drawing.Point(14, 123);
+            this.txtAddress.Multiline = true;
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(315, 71);
+            this.txtAddress.TabIndex = 19;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(588, 171);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 21;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(507, 171);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 22;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Location = new System.Drawing.Point(426, 171);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(75, 23);
+            this.btnEdit.TabIndex = 23;
+            this.btnEdit.Text = "Edit";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            // 
+            // dgStudents
+            // 
+            this.dgStudents.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgStudents.Location = new System.Drawing.Point(14, 209);
+            this.dgStudents.Name = "dgStudents";
+            this.dgStudents.Size = new System.Drawing.Size(649, 277);
+            this.dgStudents.TabIndex = 24;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(677, 498);
+            this.Controls.Add(this.dgStudents);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.lblAdress);
+            this.Controls.Add(this.txtAddress);
+            this.Controls.Add(this.lblEyeColor);
+            this.Controls.Add(this.pnlEyeColor);
+            this.Controls.Add(this.lblMobile);
+            this.Controls.Add(this.txtMobile);
+            this.Controls.Add(this.chkMarried);
+            this.Controls.Add(this.grpGender);
+            this.Controls.Add(this.lblDOB);
+            this.Controls.Add(this.dtDOB);
+            this.Controls.Add(this.picImage);
+            this.Controls.Add(this.lblDept);
+            this.Controls.Add(this.cmbDept);
+            this.Controls.Add(this.lblFaculty);
+            this.Controls.Add(this.cmbFaculty);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.lblNo);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.txtNo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.picImage)).EndInit();
+            this.grpGender.ResumeLayout(false);
+            this.grpGender.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgStudents)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNo;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label lblNo;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.ComboBox cmbFaculty;
+        private System.Windows.Forms.Label lblFaculty;
+        private System.Windows.Forms.Label lblDept;
+        private System.Windows.Forms.ComboBox cmbDept;
+        private System.Windows.Forms.PictureBox picImage;
+        private System.Windows.Forms.DateTimePicker dtDOB;
+        private System.Windows.Forms.Label lblDOB;
+        private System.Windows.Forms.RadioButton rdMale;
+        private System.Windows.Forms.RadioButton rdFemale;
+        private System.Windows.Forms.GroupBox grpGender;
+        private System.Windows.Forms.CheckBox chkMarried;
+        private System.Windows.Forms.Label lblMobile;
+        private System.Windows.Forms.TextBox txtMobile;
+        private System.Windows.Forms.Panel pnlEyeColor;
+        private System.Windows.Forms.Label lblEyeColor;
+        private System.Windows.Forms.Label lblAdress;
+        private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.DataGridView dgStudents;
+    }
+}
+
---
+++ StudentsAppV1/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StudentsAppV1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void CmbFaculty_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+            cmbDept.Items.Clear();
+            if (cmbFaculty.SelectedIndex == 0)
+            {
+                cmbDept.Items.Add("f1 options");
+                cmbDept.Items.Add("f1 options");
+                cmbDept.Items.Add("f1 options");
+            }
+            else if (cmbFaculty.SelectedIndex == 1)
+            {
+                cmbDept.Items.Add("f2 options");
+                cmbDept.Items.Add("f2 options");
+                cmbDept.Items.Add("f2 options");
+            }
+            else if (cmbFaculty.SelectedIndex == 2)
+            {
+                cmbDept.Items.Add("f3 options");
+                cmbDept.Items.Add("f3 options");
+                cmbDept.Items.Add("f3 options");
+
+            }
+        }
+
+        private void PicImage_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Title = "Choose student image";
+            ofd.Filter = "Images|*.jpg;*.tiff;*.png;*.bmp;";
+            DialogResult res = ofd.ShowDialog();
+            if (res == DialogResult.OK)
+            {
+                picImage.Image = Image.FromFile(ofd.FileName);
+            }
+
+        }
+
+
+
+        private void PnlEyeColor_Click(object sender, EventArgs e)
+        {
+            ColorDialog cd = new ColorDialog();
+            DialogResult res = cd.ShowDialog();
+            if (res == DialogResult.OK)
+            {
+                pnlEyeColor.BackColor = cd.Color;
+            }
+        }
+    }
+}
